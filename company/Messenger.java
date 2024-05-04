3ca06e57dba663028c661bd9648c3968edd36bf3
MikolajKa <miki@radzymin.com.pl>
2024-05-04 11:47:57 +0200

    dodanie

--- company/Messenger.java
+++ company/Messenger.java
@@ -1,4 +1,11 @@
+package company;
 
 public enum Messenger {
+    ;
+
+    void sendMessage(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sendMessage'");
+    }
 
 }
