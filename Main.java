d0d209e3b9ce39697642c18f5e0de469016d128c
MikolajKa <miki@radzymin.com.pl>
2024-05-04 12:00:32 +0200

    dodanie

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
 import company.implementations.EmailMessenger;
+import company.utils.MathUtils;
 
 public class Main {
 
     public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int sum = MathUtils.add(a, b);
+        System.out.println("Suma: " + sum);
+
         EmailMessenger messenger = new EmailMessenger();
-        messenger.sendMessage("Przykładowa wiadomość e-mail");
+        messenger.sendMessage("Suma " + a + " i " + b + " wynosi: " + sum);
     }
 }
