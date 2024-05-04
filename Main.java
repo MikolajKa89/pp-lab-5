bf7bd83a28ff2d6ca23096c77a3f0d986cda8375
MikolajKa <miki@radzymin.com.pl>
2024-05-04 11:48:46 +0200

    dodanie

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import company.implementations.EmailMessenger;
 public class Main {
 
     public static void main(String[] args) {
-        Messenger messenger = new EmailMessenger();
+        EmailMessenger messenger = new EmailMessenger();
         messenger.sendMessage("Przykładowa wiadomość e-mail");
     }
 }
